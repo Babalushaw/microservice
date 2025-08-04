723b691bf988c09cfcce1e586964318af1023596
Babalu kumar <babalubt18mec011@gmail.com>
2025-08-04 20:05:55 +0530

    Implementation of fetching data for ratings and hotel for user service

--- User/src/main/java/com/microservice/user/model/Rating.java
+++ User/src/main/java/com/microservice/user/model/Rating.java
@@ -7,9 +7,58 @@ import lombok.*;
 @Getter
 @Setter
 public class Rating {
-    private String ratingId;
+    private String id;
+    private String feedBack;
+    private int rating;
     private String userId;
     private String hotelId;
-    private String feedback;
-    private int rating;
+    private Hotel hotel;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFeedBack() {
+        return feedBack;
+    }
+
+    public void setFeedBack(String feedBack) {
+        this.feedBack = feedBack;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(String hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
 }
--- User/src/main/java/com/microservice/user/serviceImpl/UserServiceImpl.java
+++ User/src/main/java/com/microservice/user/serviceImpl/UserServiceImpl.java
@@ -1,19 +1,28 @@
 package com.microservice.user.serviceImpl;
 
 import com.microservice.user.exception.ResourceNotFoundException;
+import com.microservice.user.model.Hotel;
+import com.microservice.user.model.Rating;
 import com.microservice.user.model.User;
 import com.microservice.user.repository.UserRepository;
 import com.microservice.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RestTemplate restTemplate;
     @Override
     public User saveUser(User user) {
         String randomUserId= UUID.randomUUID().toString();
@@ -40,7 +49,19 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUserById(String userId) {
-        return userRepository.findById(userId).orElseThrow(()->
+        User user= userRepository.findById(userId).orElseThrow(()->
                 new ResourceNotFoundException("User not present with given id"));
+        Rating[] ratingList= restTemplate.getForObject("http://RATING-SERVICE/ratings/user/"+userId, Rating[].class);
+
+        assert ratingList != null;
+        List<Rating> ratingHotel= Arrays.stream(ratingList).map(rating->{
+            String hotelId=rating.getHotelId();
+            ResponseEntity<Hotel> hotel=restTemplate.getForEntity("http://HOTEL-SERVICE/hotels/"+hotelId, Hotel.class);
+            rating.setHotel(hotel.getBody());
+            return rating;
+        }).collect(Collectors.toList());
+        user.setRatingList(ratingHotel);
+        return user;
+
     }
 }
